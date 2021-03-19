5ea72f6464cf186b5f60a941cfaf7151cd877d5e
LeightonGuang <leighton.guang@gmail.com>
2021-03-20 07:57:29 +0800

    adding in variables

--- c++/variables.cpp
+++ c++/variables.cpp
@@ -7,6 +7,13 @@ int main() {
   //set type of variables | name of variables | value of variables
   int tuna = 10;
 
+  int a = 4;
+  int b = 6;
+
+  int sum = a + b;
+
+  cout << sum << endl;
+
   //print with cout
   cout << tuna << endl;
 
