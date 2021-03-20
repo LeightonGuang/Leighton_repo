51c46663c1ab71dc5b1d17a73b5ea788fb331392
LeightonGuang <leighton.guang@gmail.com>
2021-03-20 08:03:05 +0800

    cleaner input

--- c++/calculator(input).cpp
+++ c++/calculator(input).cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main() {
   int num1, num2, sum;
 
-  cout << "Enter the first number you want to add\n";
+  cout << "Enter the first number you want to add\n> ";
   cin >> num1;
 
-  cout << "Enter the second number you want to add\n";
+  cout << "Enter the second number you want to add\n> ";
   cin >> num2;
 
   sum = num1 + num2;
