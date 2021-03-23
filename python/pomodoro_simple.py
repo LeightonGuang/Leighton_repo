386988a24a4ca517347e6d3680c3a0209c1fd52b
Leighton417 <leighton.guang@gmail.com>
2021-03-23 10:00:53 +0800

    refactored code and added custom time support

--- python/pomodoro_simple.py
+++ python/pomodoro_simple.py
@@ -1,11 +1,21 @@
 import time
 
+def timer(minutes):
+    minutes *= 60
+    while minutes >= 0:
+        min, sec = divmod(minutes, 60)
+        countdown = ("[" + str(min).zfill(2) + ":" + str(sec).zfill(2) + "]")
+        print (countdown + "\r", end = "")
+        time.sleep(1)
+        minutes -= 1
+
 while True:
     userInput = int(input("""
 ________________________________________
 |           [Pomodor Timer]            |
 |   [1] 25 mins work and 5 mins break  |
 |   [2] 50 mins work and 10 mins break |
+|   [3] Custom time                    |
 |   [0] to quit                        |
 |______________________________________|
 
@@ -13,57 +23,38 @@ ________________________________________
 
     #print ("\n")
     if (userInput == 1):
-        timeW = 25              #set work time to 25 minutes
-        timeW *= 60             #times by 60 to make it minutes
-
-        timeB = 5               #set break time to 5 minutes
-        timeB *= 60             #times by 60 to make it minutes
         try:
-            while timeW >= 0:
-                min, sec = divmod(timeW, 60)
-                countdown = ("[" + str(min).zfill(2) + ":" + str(sec).zfill(2) + "]")
-                print (countdown + "\r", end = "")
-                time.sleep(1)
-                timeW -= 1
-
+            timer(25)
             print ("[BREAK TIME]")
             userInput = input("press [enter] to start Break")
-
-            while timeB >= 0:
-                min, sec = divmod(timeB, 60)
-                countdown = ("[" + str(min).zfill(2) + ":" + str(sec).zfill(2) + "]")
-                print (countdown + "\r", end = "")
-                time.sleep(1)
-                timeB -= 1
+            timer(5)
 
         except KeyboardInterrupt:
             pass
 
     elif (userInput == 2):
-        timeW = 50              #set work time to 25 minutes
-        timeW *= 60             #times by 60 to make it minutes
-
-        timeB = 10               #set break time to 10 minutes
-        timeB *= 60             #times by 60 to make it minutes
         try:
-            while timeW >= 0:
-                min, sec = divmod(timeW, 60)
-                countdown = ("[" + str(min).zfill(2) + ":" + str(sec).zfill(2) + "]")
-                print (countdown + "\r", end = "")
-                time.sleep(1)
-                timeW -= 1
-
+            timer(50)
             print ("[BREAK TIME]")
             userInput = input("press [enter] to start Break")
+            timer(10)
 
-            while timeB >= 0:
-                min, sec = divmod(timeB, 60)
-                countdown = ("[" + str(min).zfill(2) + ":" + str(sec).zfill(2) + "]")
-                print (countdown + "\r", end = "")
-                time.sleep(1)
-                timeB -= 1
+        except KeyboardInterrupt:
+            pass
 
-        except:
+    elif (userInput == 3):
+        userInput = int(input("Enter work minutes\n>> "))
+        minW = userInput
+        userInput = int(input("Enter break minutes\n>> "))
+        minB = userInput
+
+        try:
+            timer(minW)
+            print ("[BREAK TIME]")
+            userInput = input("press [enter] to start Break")
+            timer(minB)
+
+        except KeyboardInterrupt:
             pass
 
     elif (userInput == 0):
