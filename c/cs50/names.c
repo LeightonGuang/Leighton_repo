a9814c6578efbf2af5029c5fce072236af98d197
Leighton417 <leighton.guang@gmail.com>
2021-04-16 17:22:20 +0800

    string linear search

---
+++ c/cs50/names.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+  char names[7][10] = {"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"};
+
+  for (int i = 0; i< 7; i++)
+  {
+    if(strcmp(names[i], "Ron") == 0)
+    {
+      printf("Found\n");
+      return 0;
+    }
+  }
+  printf("Not found\n");
+  return 1;
+}
