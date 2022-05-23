23092fe02a3ca3a8aa8a842d789ded95472ca12b
Leighton Guang <leighton.guang@gmail.com>
2022-05-24 00:44:50 +0800

    freeCodecamp first course

--- javascript/memory-game/app.js
+++ javascript/memory-game/app.js
@@ -53,15 +53,63 @@ console.log(cardArray)
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
-
-console.log(gridDisplay)
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
 function createBoard(){
-    for(let i = 0; i < 10; i++){
+    for(let i = 0; i < cardArray.length; i++){
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
-        console.log(card, i)
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        gridDisplay.append(card)
+    }
+}
+createBoard()
+
+function checkMatch(){
+    const cards = document.querySelectorAll('#grid img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+    console.log('check for match')
+
+    if(optionOneId == optionTwoId){
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('You clicked the same card')
+    }
+
+    if(cardsChosen[0] == cardsChosen[1]){
+        alert('match')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    }else{
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('try again')
+    }
+    resultDisplay.textContent = cardsWon.length
+    cardsChosen = []
+    cardsChosenIds = []
+
+    if(cardsWon.length == cardArray.length / 2){
+        resultDisplay.innerHTML = ('Congrats you match them all')
     }
 }
 
-createBoard()
+function flipCard(){
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    console.log(cardsChosen)
+    console.log(cardsChosenIds)
+    this.setAttribute('src', cardArray[cardId].img)
+    if(cardsChosen.length == 2){
+        setTimeout(checkMatch, 500)
+    }
+}
